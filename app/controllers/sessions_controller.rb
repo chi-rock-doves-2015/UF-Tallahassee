e8926c975830acabbe09679280638cf4db246ae7
tmpbtz <powers.timothy@gmail.com>
2015-04-29 15:58:31 -0500

    Build basic Users and Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by_username(params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      @errors = ["You have entered an invalid username/password"]
+      render "new"
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to "root"
+  end
+end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
- def create
-   @user = User.new(user_params)
-   if @user.save
-    session[:user_id] = @user.id
-    redirect_to user_
- end
+  ### NEED VALIDATION THAT THE USER IS AFFLIATED W/UFT
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @errors = @user.errors.full_messages
+      render "new"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :role, :password)
+    end
 end
