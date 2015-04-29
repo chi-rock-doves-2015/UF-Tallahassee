4fcbe34c1c28c2e30ca7e306ad2ba99d0a902a1e
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-04-29 16:06:03 -0500

    add few relationships for user and experiment

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,2 +1,4 @@
 class Experiment < ActiveRecord::Base
+  has_many :comments, as: :commentable
+  # belongs_to :
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :
+  has_many :proposals#, foreign_key: :faculty_id
+  has_many :experiments#, foreign_key: :researcher_id
+  has_many :comments#, foreign_key: :author_id
 end
