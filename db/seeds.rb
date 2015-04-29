aee04034f5cb7bf33e7bcd10f61a4fafa8913b4f
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-04-29 18:12:30 -0500

    add seeds and add faker gem in development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+role = ["researcher", "faculty"]
+
+5.times { User.create!(name: Faker::Name.name, email: Faker::Internet.user_name + "@uft.edu", password: "password", role: role.sample) }
+
+5.times { Proposal.create!(title: Faker::Name.title, hypothesis: Faker::Lorem.paragraph, background: Faker::Lorem.paragraph, faculty_id: (1..5)) }
+
+5.times { Experiment.create!(title: Faker::Name.title, methodology: Faker::Lorem.paragraph, observations: Faker::Lorem.paragraph, conclusion: Faker::Lorem.paragraph, researcher_id: (1..5), proposal_id: (1..5)) }
+
+5.times { Comment.create!(body: Faker::Lorem.sentence, author_id: (1..5)) }
