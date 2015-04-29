1088a6e226778546b44670364422829f3d256635
Erik <erikdstock@users.noreply.github.com>
2015-04-29 17:57:31 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /.+[@uft.edu]/, message: "invalid email" }
-  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, uniqueness: true, presence: true, format: { with: /.+[@uft.edu]/, message: "invalid email" }
 
   has_secure_password
 end
