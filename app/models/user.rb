85c7c3350cb6f4ae7c02baa3c27355fb70d700d6
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-04-29 17:23:51 -0500

    update email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   has_many :comments
 
   validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /.+[@uft.edu]/, message: "invalid email" }
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 end
