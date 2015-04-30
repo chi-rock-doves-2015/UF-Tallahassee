b2ddf1152674244bfd0c70ce30c40f9ce44ad3df
Erik <erik.d.stock@gmail.com>
2015-04-29 19:59:14 -0500

    complete signin spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
 
-RSpec.feature "SignIns", type: :feature do
+RSpec.feature "Sign In", type: :feature do
+
   feature "user signs in" do
-    scenario 'with valid params' do
+    before(:all) do
+      User.create(name: "Prof. Fake", role: "faculty", email: "faculty@uft.edu", password: "password")
+      User.create(name: "Dr. Fake", role: "researcher", email: "researcher@uft.edu", password: "password")
+    end
+
+    scenario 'as a faculty member' do
       visit '/login'
       within('form') do
         fill_in 'Email', with: 'faculty@uft.edu'
         fill_in 'Password', with: 'password'
         click_button 'Login'
+      end
+      expect(page).to have_content("@Prof. Fake")
+    end
 
-        expect(page).to have_content()
+    scenario 'as a researcher' do
+      visit '/login'
+      within('form') do
+        fill_in 'Email', with: 'researcher@uft.edu'
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+      end
+      expect(page).to have_content('@Dr. Fake')
+    end
 
+    scenario 'as a bad login' do
+      visit '/login'
+      within('form') do
+        fill_in 'Email', with: 'researcher@uft.edu'
+        fill_in 'Password', with: 'pissword'
+        click_button 'Login'
+      end
+      expect(page).to have_content('Email/Password Incorrect')
     end
   end
 end
