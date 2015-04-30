9dde43c37e87bf1a5b0b92bd322a8715f91f15d5
Erik <erik.d.stock@gmail.com>
2015-04-29 22:17:21 -0500

    enable current_user as helper method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :role, :password)
