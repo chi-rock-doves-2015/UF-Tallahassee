dda66b42392a0eaac3cc4ca98f83c044f9ec33e6
tmpbtz <powers.timothy@gmail.com>
2015-05-01 15:49:33 -0500

    comments almost totally unfucked

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,31 +1,45 @@
 $(function(){
   $('.new-comment-btn').click(function(event){
     event.preventDefault();
-    var url = $(this).attr('href');
-    $.ajax({
-      url: url
-    })
+    var url = $(this).parent().attr('action');
+    // $('#new_comment').remove();
+    $.ajax({url: url})
       .done(function(response){
-        $('.main').prepend(response);
+        $('.new-comment-btn').hide();
+        $('#comments-section').prepend(response);
       })
   });
 
-//   $('main').on('submit', '#post_form', function(event){
-//     event.preventDefault();
-//     var entry = $(this).serialize();
-//     var url = $(this).attr('action');
-//     var type = "POST";
-//
-//     $.ajax({
-//       url: url,
-//       type: type,
-//       data: entry
-//     })
-//       .done(function(response1){
-//         $('#posts').prepend(response1);
-//         $('#post_form').remove();
-//         $('#new_post_link').show();
-//       })
-//   });
+  $('.reply_link').click(function(event){
+    event.preventDefault();
+    var link = this
+    var url = $(this).attr('href');
+    $.ajax({url: url})
+      .done(function(response){
+        $('#new_comment').remove();
+        debugger;
+        $(link).closest(".comments").append(response);
+        $(link).hide();
+      })
+  });
+
+  $('#comments-section').on('submit', '#new_comment', function(event){
+    event.preventDefault();
+    var entry = $(this).serialize();
+    var url = $(this).attr('action');
+    var type = "POST";
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: entry
+    })
+      .done(function(response){
+        debugger;
+        $('#new_comment').closest(".comments").prepend(response);
+        $('#new_comment').remove();
+        $('.new-comment-btn').show();
+      })
+  });
 })
 
